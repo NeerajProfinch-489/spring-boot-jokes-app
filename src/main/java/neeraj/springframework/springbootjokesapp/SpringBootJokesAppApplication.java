cec2c357e26dad4b5b57b52d2710ebb65c907abd
NeerajProfinch-489 <neeraj.patel@profinch.com>
2020-03-03 11:46:40 +0530

    commented the configuration class and done the XML configuration so get the bean note:have to give the path of original package where the outsider class present

--- src/main/java/neeraj/springframework/springbootjokesapp/SpringBootJokesAppApplication.java
+++ src/main/java/neeraj/springframework/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -1,9 +1,12 @@
 package neeraj.springframework.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-config.xml")
 public class SpringBootJokesAppApplication {
 
 	public static void main(String[] args) {
